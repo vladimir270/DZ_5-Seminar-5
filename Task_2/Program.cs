56463d8e6ebd84dbf67487060e4b892a43192a43
Владимир Ворона <vvorona665@gmail.com>
2022-06-24 22:45:18 +0300

    Completed Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте одномерный массив, заполненный случайными числами
+// Найдите сумму элементов, стоящих на нечетных позициях.
+
+
+int [] FillArray()
+{
+    int [] array = new int [6];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,10);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ,");
+    }
+    Console.WriteLine();
+}
+
+void Summa (int [] array)
+{   int s = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            s = s + array[i];
+        }
+    }
+    Console.WriteLine ($"Сумма элементов стоящих на нечетных позициях = {s}");
+}
+int [] Four = FillArray();
+PrintArray(Four);
+Summa(Four);
