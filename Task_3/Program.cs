4a83703e6f949a626bce969496004b2d27a652d2
Владимир Ворона <vvorona665@gmail.com>
2022-06-24 23:38:38 +0300

    Completed Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным 
+// элементов массива.
+
+double [] FillArray()
+{
+    double [] array = new double [6];
+    double [] array1 = new double [6];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = (new Random().NextDouble()) * (new Random().Next(1,101));
+        array1[i] = Math.Round(array[i], 2);
+    }
+    return array1;
+}
+
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ,");
+    }
+    Console.WriteLine();
+}
+
+
+void MaxMin(double [] array)
+{   double Max = array[0];
+    double Min = array[0];
+    double result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > Max)
+        {
+            Max = array[i];
+        }
+        if (array[i] < Min)
+        {
+            Min = array[i];
+        }
+        
+    }
+    result = Max - Min;
+    double result1 =Math.Round(result, 2);
+    Console.WriteLine($"Разница max-min = {result1}");
+}
+double [] Four = FillArray();
+PrintArray(Four);
+MaxMin(Four);
